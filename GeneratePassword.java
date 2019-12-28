d66557f5f2893fabf3021acb625d7446799e692c
happysushil <51189605+happysushil@users.noreply.github.com>
2019-12-28 23:15:01 +0530

    Changes DOne
    Changed

--- GeneratePassword.java
+++ GeneratePassword.java
@@ -11,7 +11,7 @@ class GeneratePassword {
 
 	protected static char[] capitalChars = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'N',
-        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a'
     };
 
 	protected static char[] numberslist = {
